5ffe24c8c5133ccc81b7ba151355379d4197e377
Jucelia Ferreira <jucelia.tecnologia@gmail.com>
2020-11-17 19:56:20 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numerador:");
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o denominador:");
+            double y = Double.Parse(Console.ReadLine());
+         
+            Divisao(x,y);
+           
+        }
+
+        private static void Divisao(double n1, double n2)
+
+        {
+            
+            
+             if (n2 == 0) 
+                {
+                Console.WriteLine("Divisão impossível");
+                }
+                else 
+                {
+                    double result = n1/n2; 
+                    Console.WriteLine($"O resultado da divisão é: " +  result.ToString("N1"));
+                }
+        }
+    }
+ 
+}
---
+++ README.mk
@@ -0,0 +1,3 @@
+#Desafio da divisão
+
+Função 	que divide 2 números reais, exibe o resultado com uma casa decimal após a vírgula.
